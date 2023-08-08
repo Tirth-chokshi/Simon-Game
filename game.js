4a0bbe5efb6ef990d3f58512cdb5de80362423c7
Tirth <122982506+T-chokshi@users.noreply.github.com>
2023-08-09 00:17:22 +0530

    final

--- game.js
+++ game.js
@@ -2,7 +2,7 @@
 var buttonColors = ["red","blue","green","yellow"];
 var gamePattern =[];
 var userClickedPattern =[];
-var level = 0;
+var level = [0];
 var started = false;
 
 $("h1").click(function() {
@@ -18,9 +18,33 @@ $(".btn").click(function(){
 
     playSound(userChosenColor);
     animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattern.length-1);
 });
-function nextSequence() {
-   level++;
+
+function checkAnswer(curreentLevel) {
+    
+    if (gamePattern[curreentLevel] === userClickedPattern[curreentLevel]) {
+        console.log("Success");
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else{
+        console.log("Dissapointment");
+        $("h1").text("Game over!");
+        $("body").addClass("game-over");
+        setTimeout(() => {
+            $("body").removeClass("game-over");
+        }, 200);
+        var audio = new Audio('sounds/wrong.mp3');
+        audio.play();
+    }
+}function nextSequence() {
+   userClickedPattern=[];
+    level++;
    $("#level-title").text("Level " + level);
    var randomNumber =  Math.floor(Math.random()* 4);
    var randomChosenColor = buttonColors[randomNumber];
@@ -28,6 +52,7 @@ function nextSequence() {
    $("#"+randomChosenColor).fadeOut(100).fadeIn(100);
    playSound(randomChosenColor);
 }
+
 
 
 
